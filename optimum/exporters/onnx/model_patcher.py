149dcf795fe8382e1a7d83e25b9b36a20c617b76
badaoui <abdennacerbadaoui0@gmail.com>
2025-08-14 16:49:31 +0200

    better fix to handle different types of configs

--- optimum/exporters/onnx/model_patcher.py
+++ optimum/exporters/onnx/model_patcher.py
@@ -462,7 +462,13 @@ class ModelPatcher:
             signature = inspect.signature(self.orig_forward)
             args, kwargs = override_arguments(args, kwargs, signature, model_kwargs=self.model_kwargs)
 
-            if is_transformers_version(">=", "4.48") or model.config["model_type"] == "nemotron": 
+            try:
+                model_type = model.config.model_type
+            except AttributeError:
+                # Handle FrozenDict case - fallback to dictionary access
+                model_type = model.config.get("model_type", None)
+
+            if is_transformers_version(">=", "4.48") or model_type == "nemotron":
                 if "past_key_values" in signature.parameters:
                     pkv_index = list(signature.parameters.keys()).index("past_key_values")
 
