c1f0c144130efe7919e813c7db3b45948eb149a8
IlyasMoutawwakil <moutawwakil.ilyas.tsi@gmail.com>
2025-09-12 14:06:44 +0200

    licences

--- optimum/exporters/onnx/_traceable_decorator.py
+++ optimum/exporters/onnx/_traceable_decorator.py
@@ -1,3 +1,16 @@
+# Copyright 2025 The HuggingFace Team. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import warnings
 from collections import defaultdict
 from functools import wraps
@@ -37,9 +50,7 @@ def traceable_check_model_inputs(func):
             for k, v in all_args["kwargs"].items():
                 all_args[k] = v
 
-        capture_flags = _CAN_RECORD_REGISTRY.get(str(self.__class__), {})  # there is a weak ref for executorch
-        if capture_flags is None:
-            capture_flags = {}
+        capture_flags = _CAN_RECORD_REGISTRY.get(str(self.__class__)) or {}  # there is a weak ref for executorch
 
         recordable_keys = {
             f"output_{k}": all_args.get(
